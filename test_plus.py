c9f83b29cd18fe6c30abd56a418b884199b400c8
Jye Ruey <rueyaa332266@gmail.com>
2022-11-24 22:47:20 +0900

    add hands_on_3

---
+++ test_plus.py
@@ -0,0 +1,13 @@
+import unittest
+from plus import plus
+
+class TestPlus(unittest.TestCase):
+
+    def test_plus_1(self):
+        self.assertEqual(2, plus(1, 1))
+
+    def test_plus_2(self):
+        self.assertEqual(0, plus(-1, 1))
+
+if __name__ == "__main__":
+    unittest.main()
